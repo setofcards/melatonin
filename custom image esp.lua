3f7213a2d7af1b3755b6f564a941749c0bd71cd7
setofcards <82534590+setofcards@users.noreply.github.com>
2024-09-24 19:28:34 -0400

    custom image esp + peter griffin

---
+++ custom image esp.lua
@@ -0,0 +1,15 @@
+local image = file.read("esp.png")
+local image_texture = utility.load_png(image_content)
+local color = ui.new_color_picker("Scripts", "Elements", "Custom Image Color", 255, 255, 255, 255) 
+
+
+function dxailwrukv()
+    local players = entity.get_players()
+    for _, players in pairs(players) do
+        local clr = ui.get(color)
+        local x, y, w, h = player:get_bbox()
+        render.image(image_texture, x, y, w, h, clr[1], clr[2], clr[3], clr[4])
+    end
+end
+
+cheat.set_callback("paint", dxailwrukv)
