e72f7e568e2489a415a98dad8efe89a19ea7ffeb
setofcards <82534590+setofcards@users.noreply.github.com>
2024-09-25 14:50:42 -0400

    in progress

---
+++ chams beta.lua
@@ -0,0 +1,8 @@
+local toggle_chams = ui.new_checkbox("Scripts", "Elements", "Toggle Chams")
+
+
+function on_paint()
+    local players = entity.get_players(false)
+
+    for _, player in pairs(players)
+end
